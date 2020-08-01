2ecc71d8bfd9b2f7338e09d53aee51300e3b3643
Mac <gitmac@runbox.com>
2020-08-01 20:06:42 +0800

    restructure main.go params

--- main.go
+++ main.go
@@ -12,14 +12,15 @@ import (
 )
 
 func main() {
-	modeServer := flag.String("server", "", "interface ip to listen from. require --scanDir")
-	modeClient := flag.String("client", "", "server ip to connect to")
+	mode := flag.String("mode", "help", "mode to run. server, client, local, check, rmdup")
+	hostIP := flag.String("hostIP", "", "server ip to host from or connect ip (server/client)")
 
 	dirPtr := flag.String("scanDir", ".", "dir to scan")
 
 	flag.Parse()
 
-	if *modeServer == "" && *modeClient == "" {
+	switch *mode {
+	case "local":
 		// local mode
 		fmt.Println("mode: local")
 
@@ -40,8 +41,7 @@ func main() {
 		// list by images
 		q := core.Queue{}
 		core.ListDirByQueue(*dirPtr, &q, false)
-
-	} else if *modeServer != "" {
+	case "server":
 		// server mode
 		fmt.Println("mode: server")
 
@@ -50,18 +50,35 @@ func main() {
 			return
 		}
 
-		err := server.Serve(*modeServer, *dirPtr)
+		err := server.Serve(*hostIP, *dirPtr)
 		if err != nil {
 			log.Fatal(err)
 		}
-
-	} else if *modeClient != "" {
+	case "client":
 		// client mode
 		fmt.Println("mode: client")
 
-		err := client.Connect(*modeClient)
+		err := client.Connect(*hostIP)
 		if err != nil {
 			log.Fatal(err)
 		}
+
+	case "help":
+	default:
+		printHelp()
 	}
+
+}
+
+func printHelp() {
+	fmt.Println(`Kagami - detect duplicate image in archive
+modes:
+  server - holds archive and send images to client to create image sums
+  client - receive images and calculate image sums
+  local - calculate image sums locally
+  check - find archives with duplicate images
+
+parameters:
+  scanDir - directory to scan archives
+  hostIP - server/client use. server: ip for server to host from. client: server ip to connect to`)
 }
